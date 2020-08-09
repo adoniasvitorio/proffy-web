0ea6aeb8f7395bddb927de7f6a3a4cde2c1bc843
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-08-09 16:40:35 -0300

    adjusts new elements in frontend

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
+import Textarea from '../../components/TextArea';
+import Select from '../../components/Select';
 
 import warningIcon from '../../assets/images/icons/warning.svg';
 
 import './styles.css';
 
 function TeacherForm() {
+
+    const [scheduleItems, setScheduleItems] = useState([
+        {week_day: 0, from: '', to: ''}
+    ]);
+
+    function addNewScheduleItem() {
+
+        setScheduleItems([
+            ...scheduleItems,
+            {week_day: 0, from: '', to: ''}
+        ]);
+    }
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -22,13 +37,59 @@ function TeacherForm() {
                    <Input name="name" label="Nome completo" />
                    <Input name="avatar" label="Avatar" />
                    <Input name="whatsapp" label="WhatsApp" />
+                   <Textarea name="bio" label="Biografia" />
                 </fieldset>
 
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                   <Input name="subject" label="Matéria" />
+                   <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            {value: 'Algoritmos', label: 'Algoritmos'},
+                            {value: 'Lógica', label: 'Lógica'},
+                            {value: 'Estrutura de dados', label: 'Estrutura de dados'},
+                            {value: 'Sistemas Operacionais', label: 'Sistemas Operacionais'},
+                            {value: 'Banco de dados', label: 'Banco de dados'},
+                            {value: 'UX & UI', label: 'UX & UI'},
+                            {value: 'Desenvolvimento Frontend', label: 'Desenvolvimento Frontend'},
+                            {value: 'Desenvolvimento Mobile', label: 'Desenvolvimento Mobile'},
+                            {value: 'Desenvolvimento Backend', label: 'Desenvolvimento Backend'},
+                        ]}
+                   />
+
                    <Input name="cost" label="Custo da sua hora por aula" />
+                </fieldset>
+                
+                <fieldset>
+                    <legend>Horários disponiveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo horario
+                        </button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem =>{
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                                <Select 
+                                    name="week_day" 
+                                    label="Dia da semana" 
+                                    options={[
+                                        {value: '0', label: 'Domingo'},
+                                        {value: '1', label: 'Segunda-feira'},
+                                        {value: '2', label: 'Terça-feira'},
+                                        {value: '3', label: 'Quarta-feira'},
+                                        {value: '4', label: 'Quinta-feira'},
+                                        {value: '5', label: 'Sexta-feira'},
+                                        {value: '6', label: 'Sábado'},
+                                    ]}
+                                />
+                        <Input name="from" label="Das" type="time" />
+                        <Input name="to" label="Até" type="time" />
+                    </div>
+                        )
+                    })}
                 </fieldset>
 
                 <footer>
--- src/pages/TeacherList/index.tsx
+++ src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import Input from '../../components/Input';
+import Select from '../../components/Select';
 
 import './styles.css';
 
@@ -11,8 +12,35 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os Proffys disponiveis.">
             <form id="search-teachers">
-                <Input name="subject" label="Matéria" />
-                <Input name="week_day" label="Dia da semana" />
+            <Select 
+                   name="subject" 
+                   label="Matéria" 
+                   options={[
+                       {value: 'Algoritmos', label: 'Algoritmos'},
+                       {value: 'Lógica', label: 'Lógica'},
+                       {value: 'Estrutura de dados', label: 'Estrutura de dados'},
+                       {value: 'Sistemas Operacionais', label: 'Sistemas Operacionais'},
+                       {value: 'Banco de dados', label: 'Banco de dados'},
+                       {value: 'UX & UI', label: 'UX & UI'},
+                       {value: 'Desenvolvimento Frontend', label: 'Desenvolvimento Frontend'},
+                       {value: 'Desenvolvimento Mobile', label: 'Desenvolvimento Mobile'},
+                       {value: 'Desenvolvimento Backend', label: 'Desenvolvimento Backend'},
+                   ]}
+                   />
+                
+                <Select 
+                   name="week_day" 
+                   label="Dia da semana" 
+                   options={[
+                       {value: '0', label: 'Domingo'},
+                       {value: '1', label: 'Segunda-feira'},
+                       {value: '2', label: 'Terça-feira'},
+                       {value: '3', label: 'Quarta-feira'},
+                       {value: '4', label: 'Quinta-feira'},
+                       {value: '5', label: 'Sexta-feira'},
+                       {value: '6', label: 'Sábado'},
+                   ]}
+                   />
                 <Input type="time" name="time" label="Hora" />
             </form>
             </PageHeader>
